46cc435438eca8315644cb5bca1bbb1d697d2135
ORIGINALrak <90960081+ORIGINALrak@users.noreply.github.com>
2024-11-14 13:28:06 +0100

    plusz izé

--- calculator/js.js
+++ calculator/js.js
@@ -1,30 +1,34 @@
 var szamsor = [];
 var szamsor2 = [];
 
+
 function szamadat(value){
     szamsor.push(value);
-    console.log(szamsor + szamsor2)
+    console.log(szamsor)
     document.getElementById('asdf').innerHTML = szamsor;
 
 }
 
 function operator(value){
-    szamsor.push(value);
-    console.log(szamsor + szamsor2)
-    document.getElementById('asdf').innerHTML = szamsor;
-
+    if(szamsor[szamsor.length-1] != "/" && szamsor[szamsor.length-1] != "-" && szamsor[szamsor.length-1] != "+" && szamsor[szamsor.length-1] != "*" && szamsor[szamsor.length-1] != ".")
+        {
+            szamsor.push(value);
+            console.log(szamsor)
+            document.getElementById('asdf').innerHTML = szamsor;
+        }
 }
 
 function szamolas(){
-    let valami = szamsor2.join('') + szamsor.join('');
+    let valami =  szamsor.join('');
     let adat = eval(valami);
     szamsor = [];
     szamsor.push(adat);
-    szamsor = [0];
+    szamsor2 = [];
     szamsor2.push(adat);
     
     document.getElementById('asdf').innerHTML = szamsor;
     document.getElementById('asdf2').innerHTML = szamsor2;
+    console.log(szamsor2)
 }
 
 function clearall(){
@@ -32,4 +36,32 @@ function clearall(){
     document.getElementById('asdf2').innerHTML = 0;
     szamsor = [];
     szamsor2 = [];
+}
+
+function pluszminusz(){
+    let minuszvalami =  szamsor.join('');
+    let minuszadat = eval(minuszvalami);
+    let asd123 = minuszadat *-1;
+    szamsor = [];
+    szamsor.push(asd123);
+    szamsor2 = [];
+    szamsor2.push(asd123);
+
+    document.getElementById('asdf').innerHTML = szamsor;
+    document.getElementById('asdf2').innerHTML = szamsor2;
+    console.log(szamsor2)
+}
+
+function masodikon(){
+    let masodikonvalami =  szamsor.join('');
+    let masodikonadat = eval(masodikonvalami);
+    let asd123 = masodikonadat * masodikonadat;
+    szamsor = [];
+    szamsor.push(asd123);
+    szamsor2 = [];
+    szamsor2.push(asd123);
+
+    document.getElementById('asdf').innerHTML = szamsor;
+    document.getElementById('asdf2').innerHTML = szamsor2;
+    console.log(szamsor2)
 }
